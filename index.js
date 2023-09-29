7ff5f49dffd1b09fb210e78c847f4cf3f724ccb4
BenBarkworth-SBG <benjamin.barkworth@skybettingandgaming.com>
2023-09-29 10:25:53 +0100

    add routes and convert HTML to EJS

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 3006;
+const port = 3000;
+
+const routes = require("./routes/routes");
+
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+// set view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Static middleware
+app.use(express.static("public/"))
+
+// Middleware for parsing URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Mount the routes
+app.use("/", routes);
+
+
+app.get('/home', (req, res) => {    
+  res.sendFile("home");
+})
+app.get('/register', (req, res) => {    
+  res.sendFile("register");
+})
+app.get('/login', (req, res) => {    
+  res.sendFile("login");
+})
+
 app.listen(port, () => {
   console.log(__dirname);
 })
-app.get('/home', (req, res) => {    
-  res.sendFile(__dirname + "/html/home.html");
-  
-})
-app.get('/aboutus', (req, res) => {    
-  res.sendFile(__dirname + "/html/aboutus.html");
-})
-app.get('/myprofile', (req, res) => {    
-  res.sendFile(__dirname + "/html/myprofile.html");
-})
-app.get('/login', (req, res) => {    
-  res.sendFile(__dirname + "/html/login.html");
-})
-
