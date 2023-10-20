574dcf3b6315c32f75872a378cb1513fbfc362a7
MattClare9743 <Matt.C.Clare@student.shu.ac.uk>
2023-10-20 16:48:11 +0100

    userCreationFE

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dataController = require('../controllers/controllers');
 const user = require('../Models/user')
+const db = require('../db')
 //const userController = require('../controllers/userController')
 
 // needed for later
@@ -44,9 +45,13 @@ router.get('/', async (req, res) => {
     console.log(req.body);
   });
 
+  //writes to DB after request from frontend
   router.post('/users', async (req, res) => {    
-    console.log(req.body);
     try {
+      const {user, email, pass} = req.body;
+      console.log(user, email, pass);
+      // do database stuff
+      db.insertUser({user, email, pass});
       res.status(201).json(req.body);
     } catch (error) {
       res.status(500).json({ error: error.message});
