fb768d1ff8c00587d31e516429b08793d6ca1cf1
MattClare9743 <Matt.C.Clare@student.shu.ac.uk>
2023-10-20 12:34:37 +0100

    user creation backend

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -4,6 +4,16 @@ const dataController = require('../controllers/controllers');
 const user = require('../Models/user')
 //const userController = require('../controllers/userController')
 
+// needed for later
+// const bodyParser = require("body-parser");
+//const bcrypt = require("bcrypt");
+
+//connecting to MongoDB database
+//const MongoClient = require("mongodb").MongoClient;
+//const url =;
+
+//const client = new MongoClient(url);
+
 router.get('/', async (req, res) => {    
     try {
       const data = await dataController.getAllData();
@@ -14,18 +24,22 @@ router.get('/', async (req, res) => {
     }
   });
   
+  //render the register page
   router.get('/register', (req, res) => {    
     res.render("register");
   });
   
+  //render the login page
   router.get('/login', (req, res) => {    
     res.render("login");
   });
   
+  //render the palette page
   router.get('/palette', (req, res) => {    
     res.render("palette");
   });
   
+  //render the palette page
   router.post('/login', (req, res) => {    
     console.log(req.body);
   });
