36dad23d77afca3ce945f414221b1c8d4d74ac22
MattClare9743 <Matt.C.Clare@student.shu.ac.uk>
2023-10-30 17:57:01 +0000

    adds get all user function

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dataController = require('../controllers/controllers');
-const user = require('../Models/user')
-const db = require('../db')
+const db = require('../db');
 
 router.get('/', async (req, res) => {    
     try {
