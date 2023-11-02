5d72dbcf4aa6e9da12d2394fecc285df0ce758bf
BenBarkworth-SBG <benjamin.barkworth@skybettingandgaming.com>
2023-11-02 14:50:09 +0000

    create Mongoose palette schema

---
+++ models/palette.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const paletteSchema = new mongoose.Schema({
+    hexCode: {
+        type: String,
+        required: true,
+        maxLength: 7
+    },    
+    type: {
+        type: String,
+        required: true,
+        enum: ['monochromatic', 'analogous', 'complementary', 'split complementary']
+    },    
+    name: {
+        type: String,
+        required: true,
+        match: /^(?! )[A-Za-z0-9\s]+$/,
+        maxLength: 20
+    }    
+},
+{collection: "Palettes"}
+);
+
+const paletteInfo = mongoose.model("paletteInfo", paletteSchema);
+
+module.exports = paletteInfo;
