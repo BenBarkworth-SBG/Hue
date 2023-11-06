f38e0c47f8169f82ca7c73de7a10059f23cdff63
BenBarkworth-SBG <benjamin.barkworth@skybettingandgaming.com>
2023-11-06 12:33:44 +0000

    remove deprecated code generating terminal messages

--- db.js
+++ db.js
@@ -24,8 +24,6 @@ const client = new MongoClient(MONGO_URI);
 const db = client.db();
 
 const conn = mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 
