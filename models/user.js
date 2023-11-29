84f1ecb2b73d58ed2e31191fde8241cef721c005
BenBarkworth-SBG <benjamin.barkworth@skybettingandgaming.com>
2023-11-29 16:41:27 +0000

    backend user registration validation

--- models/user.js
+++ models/user.js
@@ -4,19 +4,19 @@ const userSchema = new mongoose.Schema({
     user: {
         type: String,
         unique: true,
-        match: [/^(?! )[A-Za-z0-9\s]{1,30}$/, 'Must only contain letters and numbers and be less than 30 characters']
+        match: [/^(?! )[A-Za-z0-9\s]{1,30}$/, 'Username must only contain letters and numbers and be less than 30 characters']
     },
     email: {
         type: String,
         unique: true,
-        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        maxlength: 50
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Email must follow the usual format'],
+        maxlength: [50, 'Email must be less than 50 characters']
     },
     pass: String,
     favourites: [{
         paletteName: {
             type: String,
-            match: [/^(?! )[A-Za-z0-9\s]{1,20}$/, 'Must only contain letters and numbers and be less than 20 characters']
+            match: [/^(?! )[A-Za-z0-9\s]{1,20}$/, 'Password must only contain letters and numbers and be less than 20 characters']
         },
         paletteId: mongoose.Schema.Types.ObjectId,
         _id: false
